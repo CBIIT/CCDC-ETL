009e6a3e14db7f84e4f25cc58c9dca16952eb870
Wei <ywwei85@gmail.com>
2021-09-08 00:01:04 -0400

    Upload db files

---
+++ db_files/ccdc_data_resources.sql
@@ -0,0 +1,68 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ccdc
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `data_resources`
+--
+
+DROP TABLE IF EXISTS `data_resources`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `data_resources` (
+  `id` varchar(30) NOT NULL,
+  `resource_name` varchar(100) NOT NULL,
+  `resource_type` varchar(45) NOT NULL,
+  `description` varchar(2000) DEFAULT NULL,
+  `resource_uri` varchar(400) DEFAULT NULL,
+  `site_owner` varchar(200) DEFAULT NULL,
+  `poc` varchar(100) DEFAULT NULL,
+  `poc_email` varchar(200) DEFAULT NULL,
+  `api` varchar(100) DEFAULT NULL,
+  `pediatric_specific` int(1) NOT NULL,
+  `analytics` int(1) NOT NULL,
+  `visualization` int(1) NOT NULL,
+  `has_genomics_omics` int(1) NOT NULL,
+  `has_imaging_data` int(1) NOT NULL,
+  `has_clinical_data` int(1) NOT NULL,
+  `has_xenograft_data` int(1) NOT NULL,
+  `has_cell_lines_data` int(1) NOT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `status` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `data_resources`
+--
+
+LOCK TABLES `data_resources` WRITE;
+/*!40000 ALTER TABLE `data_resources` DISABLE KEYS */;
+/*!40000 ALTER TABLE `data_resources` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-09-07 23:58:04
